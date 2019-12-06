5a80e5dad690864c309a0a719dca8d983dc3e47e
Vishal Madan <VMadan@antal.com>
2019-12-06 19:11:22 +0530

    change3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {CardList} from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,24 +7,23 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      string: 'Hello Vishal Madan'
+      monsters: []
     };
+  }
+
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters: users}))
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({string: 'Hello Rayan Madan'})}>Change Text</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </header>
+      <CardList>
+      {       
+        this.state.monsters.map(monster => 
+          <h1 key={monster.id}>{monster.name}</h1>)
+      }
+      </CardList>
       </div>
     );
   }
