17b6cd0de7b534210cef00af699b07d56182a584
Vishal Madan <VMadan@antal.com>
2019-12-07 17:07:00 +0530

    change4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -16,9 +18,12 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters: users}))
   }
   render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-      <CardList monsters={this.state.monsters} />>
+      <SearchBox placeholder='search Monsters' handleChange={e => this.setState({searchField: e.target.value})}/> 
+      <CardList monsters={filteredMonsters} />
       </div>
     );
   }
