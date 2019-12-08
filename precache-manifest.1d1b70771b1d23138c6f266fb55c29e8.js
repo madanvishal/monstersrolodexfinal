5e405b476225662bde16260a898ce42d2629bdd4
Vishal Madan <VMadan@antal.com>
2019-12-08 11:10:42 +0530

    Updates

---
+++ precache-manifest.1d1b70771b1d23138c6f266fb55c29e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c37ec5a87a0cd812cc076ddd14c1998",
+    "url": "/monstersrolodexfinal/index.html"
+  },
+  {
+    "revision": "6c7fcd043f737d679a0b",
+    "url": "/monstersrolodexfinal/static/css/main.cf6ddd0e.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monstersrolodexfinal/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "6c7fcd043f737d679a0b",
+    "url": "/monstersrolodexfinal/static/js/main.5d0793c6.chunk.js"
+  },
+  {
+    "revision": "bc5f96c9e64f38e71556",
+    "url": "/monstersrolodexfinal/static/js/runtime-main.2dafd166.js"
+  }
+]);
